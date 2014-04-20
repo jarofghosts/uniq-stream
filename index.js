1f2eb71ee14d649380bedc20b5ef2b39504bfffc
jesse keane <me@jessekeane.me>
2014-04-19 21:18:12 -0700

    update

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var through = require('through')
 module.exports = uniq
 
 function uniq(options) {
-  var seen = [],
-      output = []
+  var output = []
+    , seen = []
 
   options = options || {}
   options.skip = options.skip || 0
@@ -12,16 +12,18 @@ function uniq(options) {
   return through(dedupe)
 
   function dedupe(chunk) {
-    var str = chunk.toString(),
-        compare = str.slice(options.skip)
+    var str = chunk.toString()
 
-    if (options.ignoreCase) compare = compare.toLowerCase()
-    if (seen.indexOf(compare) > -1) {
-      if (!options.inverse || output.indexOf(compare) > -1) return
+    var compare = str.slice(options.skip)
+
+    if(options.ignoreCase) compare = compare.toLowerCase()
+    if(seen.indexOf(compare) > -1) {
+      if(!options.inverse || output.indexOf(compare) > -1) return
       output.push(compare)
       this.queue(str)
     }
+
     options.global ? seen.push(compare) : seen = [compare]
-    if (!options.inverse) this.queue(str)
+    if(!options.inverse) this.queue(str)
   }
 }
